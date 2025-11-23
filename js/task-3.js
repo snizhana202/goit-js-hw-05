83d65b5a97774c7acaec4ed8ddc3eede3f953446
Snizhana <snizhanapetrushka@gmail.com>
2025-11-23 12:18:14 +0100

    changes

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,5 @@
-const sortByDescendingFriendCount = users => {
-  return [...users].toSorted((a, b) => b.friends.length - a.friends.length);
-};
+const sortByDescendingFriendCount = users =>
+  [...users].toSorted((a, b) => b.friends.length - a.friends.length);
 
 console.log(
   sortByDescendingFriendCount([
